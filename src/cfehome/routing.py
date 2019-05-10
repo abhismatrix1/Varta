b5a5089a247748f198ca06e694cb14fd89e9aa7d
abhismatrix1 <abhismatrix@gmail.com>
2019-05-10 07:32:59 +0530

    first commit. Socket is working for users

---
+++ src/cfehome/routing.py
@@ -0,0 +1,24 @@
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.conf.urls import url
+from django.urls import path, re_path
+from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator, OriginValidator
+
+#from chatconsumers import ChatConsumer
+from chat.consumers import ChatConsumer
+
+
+
+application = ProtocolTypeRouter({
+    # Empty for now (http->django views is added by default)
+    'websocket': AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+                [
+                    re_path(r"^(?P<username>[\w.@+-]+)", ChatConsumer),
+                ]
+            )
+        )
+    )
+})
+#r"^messages/(?P<username>[\w.@+-]+)/$"
